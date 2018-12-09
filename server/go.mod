4e9369c27ebcae8e72976547775305c64c0aa87a
Konstantin Wirz <konstantin.wirz@gmail.com>
2018-12-09 17:16:57 +0100

    Add a REST API and races, events and competitions endpoints

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module "https://github.com/konstantinwirz/biplan"
+
+require github.com/gorilla/mux v1.6.2
--- server/main.go
+++ server/main.go
@@ -1,30 +1,71 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	events, err := fetchEvents("1819")
+func eventHandler(w http.ResponseWriter, r *http.Request) {
+	season := mux.Vars(r)["season"]
+	events, err := fetchEvents(season)
 	if err != nil {
-		log.Fatalf("failed to fetch events: %v", err)
+		writeError(w, err.Error())
+		return
 	}
 
-	for _, event := range events {
-		fmt.Printf("%v\n", event)
-		// get competitions
-		competitions, err := fetchCompetitions(event.ID)
-		if err != nil {
-			log.Fatalf("failed to fetch competions for event '%s': %v", event.ID, err)
-		}
-		for _, comp := range competitions {
-			fmt.Printf("%v\n", comp)
-			race, err := fetchRace(comp.RaceID)
-			if err != nil {
-				log.Fatalf("failed to fetch race information for race id '%s': %v", comp.RaceID, err)
-			}
-			log.Printf("%v\n", race)
-		}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(mustMarshal(events))
+}
+
+func writeError(w http.ResponseWriter, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(500)
+	payload := map[string]string{"error": msg}
+	w.Write(mustMarshal(payload))
+}
+
+func mustMarshal(v interface{}) []byte {
+	b, err := json.Marshal(v)
+	if err != nil {
+		panic("unable to marshal")
+	}
+	return b
+}
+
+func competitionHandler(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["eventId"]
+	competitions, err := fetchCompetitions(eventID)
+	if err != nil {
+		writeError(w, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(mustMarshal(competitions))
+}
+
+func raceHandler(w http.ResponseWriter, r *http.Request) {
+	raceID := mux.Vars(r)["raceId"]
+	races, err := fetchRace(raceID)
+	if err != nil {
+		writeError(w, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(mustMarshal(races))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/events/{season}", eventHandler).Methods("GET")
+	r.HandleFunc("/v1/competitions/{eventId}", competitionHandler).Methods("GET")
+	r.HandleFunc("/v1/races/{raceId}", raceHandler).Methods("GET")
+	http.Handle("/", r)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("failed to start a http server: %v", err)
 	}
 }
